80a624b78f0f0801534750f916f435d9e80898fd
David Tomaschik <david@systemoverlord.com>
2020-01-01 00:26:27 -0800

    More progress.

--- docker/shared-webdav/nginx.sh
+++ docker/shared-webdav/nginx.sh
@@ -2,17 +2,26 @@
 
 set -ue
 
+env
+
 if [ -n "${UID-}" -a -n "${GID-}" ] ; then
-  userdel nginx
+  getent passwd nginx && userdel nginx
+  getent group nginx && groupdel nginx
   # Recreate nginx with specified UID and GID
-  useradd -u "${UID}" -U -g "${GID}" -M -o -r -s /bin/false nginx
+  groupadd -g "${GID}" -o nginx
+  useradd -u "${UID}" -g "${GID}" -M -o -r -s /bin/false nginx
+  echo "Created nginx with UID ${UID}"
 fi
 
 # setup environment
 : ${SERVER_NAME:=localhost}
 : ${AUTH_REALM:=WebDAV Server}
+export SERVER_NAME
+export AUTH_REALM
 
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$SERVER_NAME $AUTH_REALM' \
+  < /etc/nginx/nginx.conf.template \
+  > /etc/nginx/nginx.conf
 
 mkdir -p /tmp/nginx/bodies && chown -R nginx:nginx /tmp/nginx
 
