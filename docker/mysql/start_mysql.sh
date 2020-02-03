00a7f5187c885837b5acca09c021e90db21e12bd
David Tomaschik <david@systemoverlord.com>
2020-02-02 20:07:23 -0800

    fix script

--- docker/mysql/start_mysql.sh
+++ docker/mysql/start_mysql.sh
@@ -6,4 +6,5 @@ docker volume create mysql-$1
 docker run -d --name mysql-$1 \
     --mount source=mysql-$1,target=/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD=foobarbaz \
+    -p 3306:3306 \
     mysql:latest
