c6d7f725d50706b9f37c95f04099054d3f69c149
David Tomaschik <david@systemoverlord.com>
2021-01-28 20:15:40 -0800

    Start on powerlog

--- electronics/ina219.py
+++ electronics/ina219.py
@@ -19,28 +19,31 @@ class INA219B(object):
 
     def __str__(self):
         grid = []
+        max_max_i = 0
         for p in (1, 2, 4, 8):
             max_i = self.calc(p)
+            max_max_i = max(max_max_i, max_i)
             res = max_i/(2**12)
-            rv = '{:>6s}A Max, {:>6s}A/bit'.format(
+            rv = '{:>7s}A Max, {:>7s}A/bit'.format(
                     format_small_float(max_i),
                     format_small_float(res))
             grid.append(rv)
-        return '{:12s}{}'.format(
+        watts = max_max_i * max_max_i * self.resistor
+        return '{:9s} {} {:>7s}W'.format(
                 format_small_float(self.resistor) + OHM,
-                '  '.join(grid))
+                '  '.join(grid), format_small_float(watts))
 
 
 def format_small_float(f):
     if f < 1/1000:
-        return '{:3.3f}{}'.format(f*1000000, MICRO)
+        return '{:3.2f}{}'.format(f*1000000, MICRO)
     if f < 1:
-        return '{:3.3f}{}'.format(f*1000, "m")
-    return '{:3.3f}'.format(f)
+        return '{:3.2f}{}'.format(f*1000, "m")
+    return '{:3.2f}'.format(f)
 
 
 def main():
-    for r in (0.1, 0.05, 0.02):
+    for r in (0.1, 0.05, 0.025, 0.02, 0.01, 0.005):
         setup = INA219B(r)
         print(str(setup))
 
