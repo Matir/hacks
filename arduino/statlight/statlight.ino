fc4ad3c29612338901e7284c88627d669ebfbffe
David Tomaschik <david@systemoverlord.com>
2021-03-19 23:25:28 -0700

    Start on statlight

---
+++ arduino/statlight/statlight.ino
@@ -0,0 +1,73 @@
+#include <FastLED.h>
+#include <string.h>
+
+
+#define NUM_LEDS 8
+#define DATA_PIN 11
+
+typedef union {
+  void *data;
+  CRGB val;
+} funcArg;
+
+typedef struct {
+  String cmd;
+  void (*func)(String args, funcArg arg);
+  funcArg arg;
+} cmdSet;
+
+String splitFirstWord(String arg);
+void setAll(String unused_args, funcArg arg);
+
+cmdSet Commands[] = {
+  {"off", setAll, {.val = CRGB::Black}},
+  {"red", setAll, {.val = CRGB::Red}},
+  {"blue", setAll, {.val = CRGB::Blue}},
+  {"green", setAll, {.val = CRGB::Green}},
+  {"yellow", setAll, {.val = CRGB::Yellow}},
+  {"white", setAll, {.val = CRGB::White}},
+  {"", NULL, {}},
+};
+
+CRGB led_data[NUM_LEDS];
+
+void setup() {
+  Serial.begin(115200);
+  LEDS.addLeds<WS2812, DATA_PIN, RGB>(led_data, NUM_LEDS);
+  LEDS.setBrightness(128);
+}
+
+void loop() {
+  String argv = Serial.readStringUntil('\n');
+  if (argv != "") {
+    handleCmd(argv);
+  }
+}
+
+void handleCmd(String argv) {
+  String cmd = splitFirstWord(argv);
+  int i=0;
+  while (Commands[i].func) {
+    if (Commands[i].cmd.equals(cmd)) {
+      Commands[i].func(argv, Commands[i].arg);
+      FastLED.show();
+      break;
+    }
+    i++;
+  };
+}
+
+String splitFirstWord(String arg) {
+  int pos = arg.indexOf(' ');
+  if (pos == -1) {
+    return arg;
+  }
+  String rv = arg.substring(0, pos);
+  rv.remove(0, pos+1);
+  return rv;
+}
+
+void setAll(String unused_args, funcArg arg) {
+  for(int i=0;i<NUM_LEDS;i++)
+    led_data[i] = arg.val;
+}
