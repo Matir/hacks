ff7c6fb5c94c6c16d51a2cd5b26f979f8e5b4c83
David Tomaschik <david@systemoverlord.com>
2021-09-05 14:31:23 -0700

    Update define

--- go/tracer/ptrace/syscalldefine.sh
+++ go/tracer/ptrace/syscalldefine.sh
@@ -10,13 +10,25 @@ else
     grep -vP 'arch/(?!x86/)' | \
     xargs -L1 indent -st -l5000 | \
     grep -E '^[[:space:]]*(COMPAT_)?SYSCALL_DEFINE' | \
-    sed 's/^[ \t]*\(COMPAT_\)*//' | sort -u > syscalls.txt
+    sed 's/^[ \t]*\(COMPAT_\)*//' | \
+    sed -e 's/compat_ulong_t/unsigned long/g' \
+      -e 's/ compat_/ /g' -e 's/ __kernel_/ /g' \
+      -e 's/ u32,/ unsigned int,/g' -e 's/ const / /g' | \
+    sort -u > syscalls.txt
 fi
+
+# For the moment, we arbitrarily select the first from duplicates.
+SEEN_SC=""
 
 while read line ; do
   #echo ${line}
   NUMARGS="$(echo $line | sed 's/SYSCALL_DEFINE\([0-9]*\).*/\1/')"
   NAME="SYS_$(echo $line | sed 's/.*(\([a-z0-9_]\+\)[,)].*/\1/' | tr '[:lower:]' '[:upper:]')"
+  if echo "${SEEN_SC}" | grep -q " ${NAME}" ; then
+    continue
+  fi
+  LCNAME="$(echo $line | sed 's/.*(\([a-z0-9_]\+\)[,)].*/\1/')"
+  SEEN_SC="${SEEN_SC} ${NAME}"
   ARGS="$(echo $line | sed 's/[^,]*, \(.*\))/\1/')"
   echo "$NAME: $NUMARGS $ARGS"
 done < syscalls.txt
